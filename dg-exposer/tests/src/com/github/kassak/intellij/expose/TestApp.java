5018b83b1d7364793105de51d26aecc430bd3d62
Alexander Kass <Alexander.Kass@jetbrains.com>
2019-01-25 18:54:24 +0300

    test app singleton

--- dg-exposer/tests/src/com/github/kassak/intellij/expose/TestApp.java
+++ dg-exposer/tests/src/com/github/kassak/intellij/expose/TestApp.java
@@ -1,13 +1,50 @@
 package com.github.kassak.intellij.expose;
 
+import com.intellij.database.dataSource.DataSourceStorage;
+import com.intellij.database.dataSource.LocalDataSource;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.testFramework.PlatformTestCase;
+import com.intellij.testFramework.PlatformTestUtil;
 import com.intellij.util.TimeoutUtil;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.QueryStringDecoder;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.ide.BuiltInServerManager;
+import org.jetbrains.ide.HttpRequestHandler;
+import org.jetbrains.ide.RestService;
+
+import java.awt.*;
+import java.io.IOException;
 
 public class TestApp extends PlatformTestCase {
   public void testApp() {
+    Registry.get("ide.rest.api.requests.per.minute").setValue(3_000_000);
+    PlatformTestUtil.registerExtension(HttpRequestHandler.Companion.getEP_NAME(), new RestService() {
+
+      @NotNull
+      @Override
+      protected String getServiceName() {
+        return "TestApp";
+      }
+
+      @Override
+      protected boolean isMethodSupported(@NotNull HttpMethod httpMethod) {
+        return true;
+      }
+
+      @Override
+      public String execute(@NotNull QueryStringDecoder decoder, @NotNull FullHttpRequest request, @NotNull ChannelHandlerContext context) throws IOException {
+        sendOk(request, context);
+        return null;
+      }
+    }, getTestRootDisposable());
     System.out.println("TestApp is running on port: " + BuiltInServerManager.getInstance().getPort());
     System.out.flush();
-    TimeoutUtil.sleep(5_000_000);
+    LocalDataSource ds = new LocalDataSource("identifier.sqlite", "", "sqlite:identifier.sqlite", null, null);
+    DataSourceStorage.getProjectStorage(getProject()).addDataSource(ds);
+    SecondaryLoop loop = Toolkit.getDefaultToolkit().getSystemEventQueue().createSecondaryLoop();
+    loop.enter();
   }
 }
--- dg-integration/test/test.py
+++ dg-integration/test/test.py
@@ -1,11 +1,24 @@
 import unittest
 from intellij.dgapi import *
-from intellij.discover import any_instance, create_instance
+from intellij.discover import any_instance, create_instance, discover_running_instances, Client
+
+def find_test_app():
+    insts = discover_running_instances()
+    print(insts)
+    for inst in insts:
+        c = Client(inst.host, inst.port)
+        try:
+            c.perform('TestApp')
+            return c
+        except Exception as e:
+            print('Not test:', inst, e)
+    return run_test_app()
 
 def run_test_app():
     trigger = 'TestApp is running on port: '
-    import subprocess, os
+    import subprocess, os, atexit
     proc = subprocess.Popen(['gradle', 'test'], cwd=os.path.relpath('../dg-exposer', os.path.abspath(__file__)), stdout=subprocess.PIPE)
+    atexit.register(proc.terminate)
     for line in proc.stdout:
         line = line.decode("utf-8")
         idx = line.find(trigger)
@@ -15,10 +28,10 @@ def run_test_app():
             return create_instance('127.0.0.1', port)
 
 class TestDBAPI(unittest.TestCase):
-    _test_instance = run_test_app()
+    _test_instance = find_test_app()
+    _test_instance.noisy = True
 
     def test_simple(self):
-        TestDBAPI._test_instance.noisy = True
         with connect(dsn='identifier.sqlite', inst=TestDBAPI._test_instance) as c:
             with c.cursor() as cur:
                 cur.execute('create table a(a int)')
@@ -26,7 +39,6 @@ class TestDBAPI(unittest.TestCase):
                 c.rollback()
 
     def test_fetch_one(self):
-        TestDBAPI._test_instance.noisy = True
         with connect(dsn='identifier.sqlite', inst=TestDBAPI._test_instance) as c:
             with c.cursor() as cur:
                 cur.execute('select ?, ?', ('mama', 'papa'))
@@ -34,7 +46,6 @@ class TestDBAPI(unittest.TestCase):
                 self.assertIsNone(cur.fetchone())
 
     def test_fetch_one_1(self):
-        TestDBAPI._test_instance.noisy = True
         with connect(dsn='identifier.sqlite', inst=TestDBAPI._test_instance) as c:
             with c.cursor() as cur:
                 cur.execute('select \'mama\', \'papa\'')
@@ -42,7 +53,6 @@ class TestDBAPI(unittest.TestCase):
                 self.assertIsNone(cur.fetchone())
 
     def test_fetch_one_2(self):
-        TestDBAPI._test_instance.noisy = True
         with connect(dsn='identifier.sqlite', inst=TestDBAPI._test_instance) as c:
             with c.cursor() as cur:
                 cur.execute('select ? union select ?', ('mama', 'papa'))
@@ -51,7 +61,6 @@ class TestDBAPI(unittest.TestCase):
                 self.assertIsNone(cur.fetchone())
 
     def test_fetch_all(self):
-        TestDBAPI._test_instance.noisy = True
         with connect(dsn='identifier.sqlite', inst=TestDBAPI._test_instance) as c:
             with c.cursor() as cur:
                 cur.execute('select ? union select ?', ('mama', 'papa'))
@@ -59,14 +68,12 @@ class TestDBAPI(unittest.TestCase):
                 self.assertEqual([], cur.fetchall())
 
     def test_nextset(self):
-        TestDBAPI._test_instance.noisy = True
         with connect(dsn='identifier.sqlite', inst=TestDBAPI._test_instance) as c:
             with c.cursor() as cur:
                 cur.execute('select ?, ?', ('mama', 'papa'))
                 self.assertIsNone(cur.nextset())
 
     def test_describe(self):
-        TestDBAPI._test_instance.noisy = True
         with connect(dsn='identifier.sqlite', inst=TestDBAPI._test_instance) as c:
             with c.cursor() as cur:
                 cur.execute('select ? as m, ? as p', ('mama', 'papa'))
@@ -74,13 +81,11 @@ class TestDBAPI(unittest.TestCase):
                 self.assertEqual(['m', 'p'], [p[0] for p in cur.description])
 
     def test_commit2(self):
-        TestDBAPI._test_instance.noisy = True
         with connect(dsn='identifier.sqlite', inst=TestDBAPI._test_instance) as c:
             c.commit()
             c.commit()
 
     def test_type(self):
-        TestDBAPI._test_instance.noisy = True
         with connect(dsn='pg', inst=TestDBAPI._test_instance) as c:
             with c.cursor() as cur:
                 cur.execute('select now()')
